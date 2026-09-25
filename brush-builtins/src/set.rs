3db204e42390c9fb12bf9be50568e46ce093cb01
Aditya Salunkhe <aditya.salunkh919@gmail.com>
2026-09-25 16:36:24 +0530

    set refuses an option letter it does not have instead of taking it as a positional parameter

--- brush-builtins/src/set.rs
+++ brush-builtins/src/set.rs
@@ -167,6 +167,26 @@ impl builtins::Command for SetCommand {
                 now_parsing_positional_args = true;
             }
 
+            // An option letter set does not have is refused, as bash does, rather than taken as a
+            // positional parameter.
+            if i > 0
+                && !now_parsing_positional_args
+                && let Some(unknown) = arg
+                    .chars()
+                    .skip(1)
+                    .find(|c| !"abefhkmnptuvxBCEHPTo".contains(*c))
+            {
+                let mut error = clap::Error::new(clap::error::ErrorKind::UnknownArgument);
+                error.insert(
+                    clap::error::ContextKind::InvalidArg,
+                    clap::error::ContextValue::String(format!(
+                        "{}{unknown}",
+                        arg.chars().next().unwrap_or('-')
+                    )),
+                );
+                return Err(error);
+            }
+
             if let Some(plus_options) = arg.strip_prefix("+") {
                 next_arg_is_option_value = plus_options.ends_with('o');
                 for c in plus_options.chars() {
